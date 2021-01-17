ffc507102154a24a4f9b02a09ee835c6b336b948
Siegmentation Fault <siegmentationfault@yandex.ru>
2021-01-17 22:33:45 +0700

    fix

--- scripts/cheats.py
+++ scripts/cheats.py
@@ -1,4 +1,5 @@
 from piqueserver.commands import command
+from piqueserver.core_commands.movement import do_move
 
 @command(admin_only=True)
 def elevate(conn, *args):
@@ -7,8 +8,7 @@ def elevate(conn, *args):
     x, y, _ = conn.world_object.position.get()
     z = conn.protocol.map.get_z(x, y)
 
-    conn.world_object.set_position(x, y, z)
-    conn.on_position_update()
+    do_move(conn, (x, y, z), silent=True)
 
 def apply_script(protocol, connection, config):
     return protocol, connection
