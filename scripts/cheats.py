f4801526f1e92be5d09210656d2cdc44bfef5a64
Siegmentation Fault <siegmentationfault@yandex.ru>
2021-01-17 22:28:15 +0700

    upload

---
+++ scripts/cheats.py
@@ -0,0 +1,11 @@
+from piqueserver.commands import command
+
+@command(admin_only=True)
+def elevate(conn, *args):
+    if not conn.hp: return
+
+    x, y, _ = conn.world_object.position.get()
+    z = conn.protocol.map.get_z(x, y)
+
+    conn.world_object.set_position(x, y, z)
+    conn.on_position_update()
