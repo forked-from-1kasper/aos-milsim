5c5cdbc5eda9b7f34c8c45c5574a057fe1e0d966
Siegmentation Fault <siegmentationfault@yandex.ru>
2023-09-14 18:21:35 +0700

    /mail

--- scripts/toolbox.py
+++ scripts/toolbox.py
@@ -1,5 +1,8 @@
-from piqueserver.commands import command
+from piqueserver.commands import command, join_arguments
 from piqueserver.config import config
+
+from time import time
+from math import inf
 
 @command(admin_only=True)
 def elevate(conn, *args):
@@ -22,9 +25,35 @@ description = discord.option("description", "Discord").get()
 def discord(conn, *args):
     return "%s: %s" % (description, invite)
 
+mailbox   = config.section("mailbox")
+mailfile  = mailbox.option("file", "mailbox.txt").get()
+maildelay = mailbox.option("delay", 90).get()
+
+@command()
+def mail(conn, *args):
+    message = join_arguments(args)
+
+    if not message:
+        return "Do not send empty messages (admins can see your IP)."
+
+    ip, port = conn.address
+
+    timestamp = time()
+
+    dt = timestamp - conn.lastmail
+    if dt < maildelay:
+        return "Do not write too often: wait %.1f seconds." % (maildelay - dt)
+
+    with open(mailfile, 'a') as fout:
+        fout.write("%.2f: %s (%s): %s\n" % (timestamp, conn.name, ip, message))
+        conn.lastmail = timestamp
+        return "Message sent."
+
 def apply_script(protocol, connection, config):
     class ToolboxConnection(connection):
         def on_connect(self):
+            self.lastmail = -inf
+
             self.chat_limiter._seconds = 0
             return connection.on_connect(self)
 
