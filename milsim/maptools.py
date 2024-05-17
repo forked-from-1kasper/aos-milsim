54a583f30aae14edfb58174fb375748f1db14b9d
Siegmentation Fault <siegmentationfault@yandex.ru>
2024-05-17 17:04:00 +0700

    fix spawn in blocks

--- milsim/maptools.py
+++ milsim/maptools.py
@@ -6,24 +6,6 @@ from pyspades.constants import BLUE_FLAG, GREEN_FLAG, BLUE_BASE, GREEN_BASE
 
 from milsim.common import *
 
-def Confined(func):
-    neighbours = list(product(range(-3, 4), repeat = 2))
-
-    def Implementation(conn):
-        x, y, z = func(conn)
-
-        if conn.is_location_free(x, y, z):
-            return (x, y, z)
-
-        shuffle(neighbours)
-        for dx, dy in neighbours:
-            if conn.is_location_free(x + dx, y + dy, z):
-                return (x + dx, y + dy, z)
-
-        return (x, y, z)
-
-    return Implementation
-
 def failure(conn):
     return NotImplementedError
 
@@ -33,15 +15,38 @@ def Location(x, y, z):
 
     return Implementation
 
-def Rectangle(x1 = 0, y1 = 0, x2 = 512, y2 = 512, zs = None):
+def uniform2(M, xmin, xmax, ymin, ymax):
+    x = floor(uniform(xmin, xmax))
+    y = floor(uniform(ymin, ymax))
+    z = M.get_z(x, y)
+    return x, y, z
+
+def Rectangle(x1 = 0, y1 = 0, x2 = 512, y2 = 512):
     xmin, xmax = min(x1, x2), max(x1, x2)
     ymin, ymax = min(y1, y2), max(y1, y2)
 
     def Implementation(conn):
-        x = floor(uniform(xmin, xmax))
-        y = floor(uniform(ymin, ymax))
-        z = conn.protocol.map.get_z(x, y) if zs is None else choice(zs)
-        return (x, y, z)
+        return uniform2(conn.protocol.map, xmin, xmax, ymin, ymax)
+
+    return Implementation
+
+def is_location_free(M, w):
+    x, y, z = w
+    return M.get_solid(x, y, z - 3) == 0 and \
+           M.get_solid(x, y, z - 2) == 0 and \
+           M.get_solid(x, y, z - 1) == 0 and \
+           M.get_solid(x, y, z - 0) == 1
+
+def Bitmap(x1 = 0, y1 = 0, x2 = 512, y2 = 512, zs = []):
+    xmin, xmax = min(x1, x2), max(x1, x2)
+    ymin, ymax = min(y1, y2), max(y1, y2)
+
+    L0 = list(product(range(xmin, xmax + 1), range(ymin, ymax + 1), zs))
+
+    def Implementation(conn):
+        # Not the most performant solution but *seems* to be robust enough.
+        L = list(filter(lambda w: is_location_free(conn.protocol.map, w), L0))
+        return choice(L) if len(L) > 0 else uniform2(conn.protocol.map, xmin, xmax, ymin, ymax)
 
     return Implementation
 
