801580c0394c067fe896e3f850c6c0f54716b026
Siegmentation Fault <siegmentationfault@yandex.ru>
2024-08-24 16:55:00 +0700

    bugfix

--- scripts/idpool.py
+++ scripts/idpool.py
@@ -1,14 +1,17 @@
 from itertools import count, filterfalse
 
-class IDPool:
+class IDPool(set):
     def __init__(self, protocol):
         self.protocol = protocol
+        super().__init__()
 
     def pop(self):
-        return next(filterfalse(self.protocol.players.__contains__, count()))
+        ID = next(filterfalse(self.__contains__, count()))
+        self.add(ID)
+        return ID
 
-    def put_back(self, id):
-        pass
+    def put_back(self, ID):
+        self.remove(ID)
 
 def apply_script(protocol, connection, config):
     class IDPoolProtocol(protocol):
