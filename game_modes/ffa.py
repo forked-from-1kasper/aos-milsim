c8ed630f220053bf33a737c0670d553aece62f68
rzrn <rzrngh@outlook.com>
2026-01-29 15:48:23 +0700

    game_modes/ffa.py: clean up

--- game_modes/ffa.py
+++ game_modes/ffa.py
@@ -1,5 +1,5 @@
 # Copyright © 2012 Youself
-# Copyright © 2021 rzrn
+# Copyright © 2021, 2026 rzrn
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,53 +14,47 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from pyspades.constants import (
-    TEAM_CHANGE_KILL, CLASS_CHANGE_KILL
-)
-from piqueserver.commands import command
-from pyspades.constants import CTF_MODE
-from twisted.internet import reactor
 from dataclasses import dataclass
 from itertools import islice
 from random import randint
+from math import inf
 
-fst = lambda xs: xs[0]
-snd = lambda xs: xs[1]
+from twisted.internet import reactor
 
-IGNORE_IN_TOP = [TEAM_CHANGE_KILL, CLASS_CHANGE_KILL]
-TOP_FREQUENCY = 120
-TOP_SIZE = 3
+from pyspades.constants import CTF_MODE, TEAM_CHANGE_KILL, CLASS_CHANGE_KILL
+from piqueserver.commands import command
+from piqueserver.config import config
 
-# If ALWAYS_ENABLED is False, free for all can still be enabled in the map
-# metadata by setting the key 'free_for_all' to True in the extensions
-# dictionary
-ALWAYS_ENABLED = True
-
-# If WATER_SPAWNS is True, then players can spawn in water
-WATER_SPAWNS = False
-
-HIDE_POS = (0, 0, 63)
-
-def show_score(item):
-    idx, (name, score) = item
-    return "%d) %s (%d kills, %d deaths)" % (idx, name, score.kills, score.deaths)
+ffa_section       = config.section("ffa")
+ffa_top_size      = ffa_section.option("top_size", 3).get()
+ffa_top_frequency = ffa_section.option("top_frequency", 120).get()
 
 def game_top(protocol):
     nicknames = sorted(
         protocol.scores.items(),
-        key = lambda item: snd(item).value(),
+        key = lambda item: item[1].value(),
         reverse = True
     )
-    top = enumerate(nicknames[:TOP_SIZE], start=1)
+
+    top = enumerate(nicknames[:ffa_top_size], start = 1)
 
     if len(nicknames) > 0:
-        return "Top players\n" + "\n".join(map(show_score, top))
+        return "Top players\n" + "\n".join(
+            "{}) {} ({} kills, {} deaths)".format(
+                no, nickname, value.kills, value.deaths
+            )
+            for no, (nickname, value) in top
+        )
     else:
-        return "No players today."
+        return "No players today"
 
-@command('scores')
-def scores(conn, *args):
-    return game_top(conn.protocol)
+@command("scores", "score")
+def c_scores(connnection):
+    """
+    Report the current score table
+    /scores
+    """
+    return game_top(connnection.protocol)
 
 @dataclass
 class Score:
@@ -72,9 +66,9 @@ class Score:
 
 def apply_script(protocol, connection, config):
     class FreeForAllProtocol(protocol):
-        game_mode = CTF_MODE
-        free_for_all = False
-        old_friendly_fire = None
+        game_mode    = CTF_MODE
+        free_for_all = True
+        hide_coord   = (inf, inf, 128)
 
         def __init__(self, *arg, **kw):
             self.scores = {}
@@ -89,38 +83,22 @@ def apply_script(protocol, connection, config):
             self.spawn_borders_x = extensions.get('spawn_borders_x', (0, 511))
             self.spawn_borders_y = extensions.get('spawn_borders_y', (0, 511))
 
-            if ALWAYS_ENABLED:
-                self.free_for_all = True
-            else:
-                if 'free_for_all' in extensions:
-                    self.free_for_all = extensions['free_for_all']
-                else:
-                    self.free_for_all = False
-            if self.free_for_all:
-                self.old_friendly_fire = self.friendly_fire
-                self.friendly_fire = True
-            else:
-                if self.old_friendly_fire is not None:
-                    self.friendly_fire = self.old_friendly_fire
-                    self.old_friendly_fire = None
+            self.friendly_fire = True
 
             return protocol.on_map_change(self, map)
 
         def on_base_spawn(self, x, y, z, base, entity_id):
-            if self.free_for_all:
-                return HIDE_POS
-            return protocol.on_base_spawn(self, x, y, z, base, entity_id)
+            return self.hide_coord
 
         def on_flag_spawn(self, x, y, z, flag, entity_id):
-            if self.free_for_all:
-                return HIDE_POS
-            return protocol.on_flag_spawn(self, x, y, z, flag, entity_id)
+            return self.hide_coord
 
-        def send_top(self, init=False):
-            if not init:
+        def send_top(self, init = False):
+            if init is False:
                 for line in reversed(game_top(self).split('\n')):
                     self.broadcast_chat(line)
-            reactor.callLater(TOP_FREQUENCY, self.send_top)
+
+            reactor.callLater(ffa_top_frequency, self.send_top)
 
     class FreeForAllConnection(connection):
         score_hack = False
@@ -131,46 +109,43 @@ def apply_script(protocol, connection, config):
                     x = randint(*self.protocol.spawn_borders_x)
                     y = randint(*self.protocol.spawn_borders_y)
                     z = self.protocol.map.get_z(x, y)
-                    if z != 63 or WATER_SPAWNS:
-                        break
+
+                    if z < 63: break
+
                 # Magic numbers taken from server.py spawn function
                 z -= 2.4
                 x += 0.5
                 y += 0.5
-                return (x, y, z)
+                return x, y, z
+
             return connection.on_spawn_location(self, pos)
 
         def on_refill(self):
-            if self.protocol.free_for_all:
-                return False
-            return connection.on_refill(self)
+            return False
 
         def on_flag_take(self):
-            if self.protocol.free_for_all:
-                return False
-            return connection.on_flag_take(self)
+            return False
 
         def on_login(self, name):
             if name not in self.protocol.scores:
                 self.protocol.scores[name] = Score()
-            return connection.on_login(self, name)
 
-        def on_kill(self, by, reason, grenade):
-            if by is not None and self.name != by.name:
-                self.protocol.scores[by.name].kills += 1
-            if reason not in IGNORE_IN_TOP:
+            connection.on_login(self, name)
+
+        def on_kill(self, hit_by, kill_type, grenade):
+            if hit_by is not None and self.name != hit_by.name:
+                self.protocol.scores[hit_by.name].kills += 1
+
+            if kill_type not in {TEAM_CHANGE_KILL, CLASS_CHANGE_KILL}:
                 self.protocol.scores[self.name].deaths += 1
 
             # Switch teams to add score hack
-            if by is not None and by.team is self.team and self is not by:
+            if hit_by is not None and hit_by.team is self.team and self is not hit_by:
                 self.score_hack = True
-                pos = self.world_object.position
                 self.set_team(self.team.other)
-                self.spawn((pos.x, pos.y, pos.z))
+                self.spawn(self.world_object.position.get())
                 self.score_hack = False
-            return connection.on_kill(self, by, reason, grenade)
+
+            return connection.on_kill(self, hit_by, kill_type, grenade)
 
     return FreeForAllProtocol, FreeForAllConnection
-
-# x : from 238 to 270
-# y : from 99 to 424
