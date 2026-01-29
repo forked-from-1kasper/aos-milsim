0c4748d34f8400ceb635c355e6c31ab27d7f50b8
rzrn <rzrngh@outlook.com>
2026-01-29 15:01:20 +0700

    milsim/grammar/: AGPL

--- milsim/grammar/__init__.py
+++ milsim/grammar/__init__.py
@@ -1,3 +1,18 @@
+# Copyright © 2026 rzrn
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from collections.abc import Iterator
 
 from milsim.grammar.category import (
@@ -10,6 +25,7 @@ from milsim.grammar.category import (
 )
 
 from milsim.grammar.verb import Verb, ModalVerb, SemiregularVerb, RegularVerb
+
 from milsim.grammar.noun import Noun, SemiregularNoun, RegularNoun
 
 from milsim.grammar.syntax import HasEmit, Token, Phrase, NP, VP, Sentence
