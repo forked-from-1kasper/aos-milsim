839759223987523da199f3c3292241e169157788
rzrn <rzrngh@outlook.com>
2026-01-29 15:02:22 +0700

    milsim/: GPL + AGPL

--- milsim/map.py
+++ milsim/map.py
@@ -1,3 +1,22 @@
+# Copyright © 2011–2012 Mathias Kaerlev
+# Copyright © 2011–2012 duckslingers
+# Copyright © 2011–2012 triplefox
+# Copyright © 2014–2018 Samuel Walladge
+# Copyright © 2024 rzrn
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from itertools import chain, islice, repeat
 from time import monotonic
 from random import randint
